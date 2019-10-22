e626d1500d7844e6a2dd5a309dbca6ac0223ccfe
Robert Gale <galer@ohsu.edu>
2019-10-22 15:20:30 -0700

    Version 0.1.0 -> 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='kaldibin',
-    version='0.1.0',
+    version='0.2.0',
     author='Robert Gale',
     author_email='rcgale@gmail.com',
     packages=[
