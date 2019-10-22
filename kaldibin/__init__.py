dbe6291f3ab6f75d980bed4b7ac4636a4ced0e50
Robert Gale <galer@ohsu.edu>
2019-10-22 15:18:05 -0700

    Adding lattice_to_ctm_conf and lattice_determinize

--- kaldibin/__init__.py
+++ kaldibin/__init__.py
@@ -4,7 +4,8 @@ import os
 from kaldibin.adapters.io import KaldiBytes, KaldiFile, KaldiGzFile, KaldiPipe
 from kaldibin.adapters.context import KaldiContext
 from kaldibin.featbin import compute_mfcc_feats
-from kaldibin.latbin import lattice_1best, lattice_align_phones, lattice_align_words_lexicon, nbest_to_ctm
+from kaldibin.latbin import lattice_1best, lattice_align_phones, lattice_align_words_lexicon, lattice_determinize,\
+    lattice_to_ctm_conf, nbest_to_ctm
 
 _KALDI_ROOT = os.environ['KALDI_ROOT'] if 'KALDI_ROOT' in os.environ else None
 _context = KaldiContext(_KALDI_ROOT)
--- kaldibin/latbin.py
+++ kaldibin/latbin.py
@@ -69,6 +69,62 @@ def lattice_align_words_lexicon(
     )
 
 
+def lattice_determinize(
+        lattice,
+        *_,
+        acoustic_sale=1.0,
+        beam=10.0,
+        beam_ratio=0.9,
+        delta=0.000976562,
+        max_loop=500000,
+        max_mem=50000000,
+        minimize=False,
+        num_loops=20,
+        prune=False
+):
+    return kaldibin._context.run(
+        "src/latbin/lattice-determinize",
+        "--acoustic-scale={}".format(acoustic_sale),
+        "--beam={}".format(beam),
+        "--beam-ratio={}".format(beam_ratio),
+        "--delta={}".format(delta),
+        "--max-loop={}".format(max_loop),
+        "--max-mem={}".format(max_mem),
+        "--minimize={}".format("true" if minimize else "false"),
+        "--num-loops={}".format(num_loops),
+        "--prune={}".format("true" if prune else "false"),
+        lattice,
+        wxtype="ark",
+        wxfilename="-"
+    )
+
+
+def lattice_to_ctm_conf(
+        lattice,
+        *_,
+        acoustic_scale=1.0,
+        confidence_digits=2,
+        decode_mbr=True,
+        frame_shift=0.01,
+        inv_acoustic_scale=1.0,
+        lm_scale=1.0,
+        print_silence=False
+):
+    return kaldibin._context.run(
+        "src/latbin/lattice-to-ctm-conf",
+        "--acoustic-scale={}".format(float(acoustic_scale)),
+        "--confidence-digits={}".format(int(confidence_digits)),
+        "--decode-mbr={}".format("true" if decode_mbr else "false"),
+        "--frame-shift={}".format(frame_shift),
+        "--inv-acoustic-scale={}".format(inv_acoustic_scale),
+        "--lm-scale={}".format(lm_scale),
+        "--print-silence={}".format("true" if print_silence else "false"),
+        lattice,
+        wxtype = None,
+        wxfilename = "-"
+    )
+
+
 def nbest_to_ctm(
         nbest,
         *_,
